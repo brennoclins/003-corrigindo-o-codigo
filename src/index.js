fa819720ba7f8a66501ad76d4b6a52040a12f0e2
Brenno C. Lins <dev@bcl-st.com.br>
2022-01-24 15:20:36 -0300

    Aluno: Brenno, Ignite NodeJS - Desafio 03 - Corrigindo o código [ok]

--- src/index.js
+++ src/index.js
@@ -23,18 +23,27 @@ app.post("/repositories", (request, response) => {
     likes: 0
   };
 
-  return response.json(repository);
+  repositories.push(...repositories, repository);
+
+  return response.status(201).json(repository);
 });
 
 app.put("/repositories/:id", (request, response) => {
   const { id } = request.params;
-  const updatedRepository = request.body;
-
-  repositoryIndex = repositories.findindex(repository => repository.id === id);
+  const { title, url, techs } = request.body;
+ 
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id);
 
   if (repositoryIndex < 0) {
     return response.status(404).json({ error: "Repository not found" });
   }
+
+  const updatedRepository = {
+    title,
+    url,
+    techs,
+    like: 0
+  };
 
   const repository = { ...repositories[repositoryIndex], ...updatedRepository };
 
@@ -48,7 +57,7 @@ app.delete("/repositories/:id", (request, response) => {
 
   repositoryIndex = repositories.findIndex(repository => repository.id === id);
 
-  if (repositoryIndex > 0) {
+  if (repositoryIndex === -1) {
     return response.status(404).json({ error: "Repository not found" });
   }
 
@@ -65,10 +74,10 @@ app.post("/repositories/:id/like", (request, response) => {
   if (repositoryIndex < 0) {
     return response.status(404).json({ error: "Repository not found" });
   }
-
+  
   const likes = ++repositories[repositoryIndex].likes;
-
-  return response.json('likes');
+  
+  return response.status(201).json({"likes": likes });
 });
 
 module.exports = app;
